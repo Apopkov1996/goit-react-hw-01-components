174d3aa5209cffd118f3831bcf079be0faa58b9b
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-10-18 17:04:23 +0300

    task 1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+export const Profile = ({ dataUser }) => {
+  const { username, tag, location, avatar, stats } = dataUser;
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt={tag} class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
